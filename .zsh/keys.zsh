86c2d12656ff6f34e48cbee2d7608096786a59de
Jan-Peter Larsson <jan-peter.larsson@arm.com>
2018-02-08 10:53:40 +0000

    Remap <v> to enter visual mode instead of opening Vim

--- .zsh/keys.zsh
+++ .zsh/keys.zsh
@@ -4,11 +4,6 @@ bindkey -v
 # backspace=2; allow deletion past insert point
 bindkey -v '^?' backward-delete-char
 
-# ESC-v opens in editor
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey -M vicmd v edit-command-line
-
 # do fuzzy matching for up/down matches
 # requires zsh-history-substring-search
 bindkey '^[[A' history-substring-search-up
